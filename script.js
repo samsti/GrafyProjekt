d9773086d06fe5cb90fbb498bb8845ab0acd5a05
Samuel Stiksa <samsti@email.cz>
2023-05-12 13:19:54 +0200

    blabla

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const tableBody = document.getElementById('table-body');
 
-// Generate 100 rows of fake data using Faker.js
 for (let i = 1; i <= 100; i++) {
     const id = i;
     const name = faker.name.findName();
     const age = faker.random.number({min: 18, max: 65});
     const money = faker.finance.amount();
 
-    // Create a new table row with the data
     const row = document.createElement('tr');
     row.innerHTML = `
 				<td>${id}</td>
 				<td>${name}</td>
 				<td>${age}</td>
-				<td>${money}</td>
+				<td>${money} $</td>
 			`;
 
-    // Add the row to the table body
     tableBody.appendChild(row);
 }
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'X');
+    data.addColumn('number', 'Series 1');
+    data.addColumn('number', 'Series 2');
+    data.addColumn('number', 'Series 3');
+    data.addRows([
+        [1, 37.8, 80.8, 41.8],
+        [2, 30.9, 69.5, 32.4],
+        [3, 25.4, 57, 25.7],
+        [4, 11.7, 18.8, 10.5],
+        [5, 11.9, 17.6, 10.4],
+        [6, 8.8, 13.6, 7.7],
+        [7, 7.6, 12.3, 9.6],
+        [8, 12.3, 29.2, 10.6],
+        [9, 16.9, 42.9, 14.8],
+        [10, 12.8, 30.9, 11.6],
+    ]);
+
+    // Set chart options
+    var options = {'title':'Chart Title',
+        'width':1000,
+        'height':500};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.LineChart(document.getElementById('chart'));
+    chart.draw(data, options);
+}
