6b075a3660d248b313e02b50aa7bff386003471e
Samuel Stiksa <samsti@email.cz>
2023-05-13 15:20:19 +0200

    I hope this is final

--- script.js
+++ script.js
@@ -1,51 +1,80 @@
 const tableBody = document.getElementById('table-body');
+const salaryCheckbox = document.getElementById('age');
+const moneyCheckbox = document.getElementById('money');
+const dataSalary = [];
+const dataMoney = [];
 
 for (let i = 1; i <= 100; i++) {
     const id = i;
     const name = faker.name.findName();
-    const age = faker.random.number({min: 18, max: 65});
-    const money = faker.finance.amount();
+    const salary = parseFloat(faker.finance.amount(500, 6000));
+    const money = parseFloat(faker.finance.amount(500, 10000));
 
     const row = document.createElement('tr');
     row.innerHTML = `
-				<td>${id}</td>
-				<td>${name}</td>
-				<td>${age}</td>
-				<td>${money} $</td>
-			`;
-
+        <td>${id}</td>
+        <td>${name}</td>
+        <td>${salary} $</td>
+        <td>${money} $</td>
+    `;
     tableBody.appendChild(row);
+
+    dataSalary.push([name, salary]);
+    dataMoney.push([name, money]);
 }
 
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', { 'packages': ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('number', 'X');
-    data.addColumn('number', 'Series 1');
-    data.addColumn('number', 'Series 2');
-    data.addColumn('number', 'Series 3');
-    data.addRows([
-        [1, 37.8, 80.8, 41.8],
-        [2, 30.9, 69.5, 32.4],
-        [3, 25.4, 57, 25.7],
-        [4, 11.7, 18.8, 10.5],
-        [5, 11.9, 17.6, 10.4],
-        [6, 8.8, 13.6, 7.7],
-        [7, 7.6, 12.3, 9.6],
-        [8, 12.3, 29.2, 10.6],
-        [9, 16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-    ]);
+    const combinedData = new google.visualization.DataTable();
+    combinedData.addColumn('string', 'name');
+    combinedData.addColumn('number', 'salary');
+    combinedData.addColumn('number', 'money');
+    for (let i = 0; i < dataSalary.length; i++) {
+        combinedData.addRow([dataSalary[i][0], dataSalary[i][1], dataMoney[i][1]]);
+    }
 
-    // Set chart options
-    var options = {'title':'Chart Title',
-        'width':1000,
-        'height':500};
+    const options = {
+        title: 'Age vs Money',
+        width: 1000,
+        height: 500,
+        vAxis: {
+            title: '',
+        },
+        hAxis: {
+            title: 'Names',
+        },
+        legend: 'none',
 
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.LineChart(document.getElementById('chart'));
-    chart.draw(data, options);
+    };
+
+    if (salaryCheckbox.checked && moneyCheckbox.checked) {
+        options.vAxis.format = '$#,##0.00';
+    } else if (salaryCheckbox.checked) {
+        options.vAxis.title = 'Salary';
+        options.vAxis.format = '$#,##0.00';
+    } else if (moneyCheckbox.checked) {
+        options.vAxis.title = 'Money';
+        options.vAxis.format = '$#,##0.00';
+    }
+
+    const chart = new google.visualization.LineChart(document.getElementById('chart'));
+
+    if (salaryCheckbox.checked && moneyCheckbox.checked) {
+        chart.draw(combinedData, options);
+    } else if (salaryCheckbox.checked) {
+        const dataTableAge = new google.visualization.DataTable();
+        dataTableAge.addColumn('string', 'name');
+        dataTableAge.addColumn('number', 'salary');
+        dataTableAge.addRows(dataSalary);
+        chart.draw(dataTableAge, options);
+
+    } else if (moneyCheckbox.checked) {
+        const dataTableMoney = new google.visualization.DataTable();
+        dataTableMoney.addColumn('string', 'name');
+        dataTableMoney.addColumn('number', 'money');
+        dataTableMoney.addRows(dataMoney);
+        chart.draw(dataTableMoney, options);
+    }
 }
